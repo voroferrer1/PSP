ddc8f914c6dc8bc18356ea0449beeb87b91dfb0e
voroferrer <2dam.vferrer@institutoserpis.org>
2018-09-18 10:32:09 +0200

    Añadido Hola Mundos

---
+++ HolaMundo4/src/HolaMundo1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class HolaMundo1{
+	public static void main(String args[] ){
+		for (int i=0;i <= 20;i++){
+			System.out.println("Hola Mundo");
+		}
+	}
+}
---
+++ HolaMundo4/src/HolaMundo2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class holaMundo2{
+	public static void main(String args[]){
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Inserta tu nombre:");
+		String nombre= entrada.next();
+		for(int i=0; i<=20; i++){
+			System.out.printf("Tu nombre es: %s\n",nombre);
+			
+		}
+	}
+}
